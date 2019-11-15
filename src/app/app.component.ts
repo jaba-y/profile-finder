4a091d8ae8f15261591052c480f632338a1df293
Jaba Yesudas <jaba.y@experionglobal.com>
2019-11-15 16:33:36 +0530

    GitHub Profile Finder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'find-profile';
+export class AppComponent implements OnInit {
+  searchForm: FormGroup;
+  data: any;
+  name: any;
+  avatarUrl: any;
+  location: any;
+  htmlUrl: any;
+  dataLoading = false;
+  dataFound = false;
+  dataNotFound = false;
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      searchText : new FormControl('')
+    });
+  }
+
+  fetchProfile(username) {
+    return this.http.get('https://api.github.com/users/' + username + '?access_token=beba3c150021bfb49769385927dfa59fac2cdf04');
+  }
+
+  onSubmit() {
+    const searchText = this.searchForm.value.searchText;
+    if (searchText) {
+      this.setDataLoading();
+      if (localStorage.getItem(searchText)) {
+        this.data = JSON.parse(localStorage.getItem(searchText));
+        this.fillData(this.data);
+      } else {
+        this.fetchProfile(searchText).subscribe(response => {
+          this.data = response;
+          this.fillData(this.data);
+          localStorage.setItem(searchText, JSON.stringify(this.data));
+        }, error => {
+          this.setDataNotFound();
+          this.openSnackBar('No Profile Found', '');
+        });
+      }
+   } else {
+     this.openSnackBar('Enter any text', '');
+   }
+ }
+
+  fillData(data) {
+    this.setDataFound();
+    this.name = data.name;
+    this.avatarUrl = data.avatar_url;
+    this.location = data.location;
+    this.htmlUrl = data.html_url;
+  }
+
+  setDataFound() {
+    this.dataFound = true;
+    this.dataNotFound = false;
+    this.dataLoading = false;
+  }
+
+  setDataNotFound() {
+    this.dataFound = false;
+    this.dataNotFound = true;
+    this.dataLoading = false;
+  }
+
+  setDataLoading() {
+    this.dataFound = false;
+    this.dataNotFound = false;
+    this.dataLoading = true;
+  }
+
+  openSnackBar(message, action) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: 'top',
+      horizontalPosition: 'right'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
